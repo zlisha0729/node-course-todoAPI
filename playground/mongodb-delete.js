5cab02619f7724a5f9d5335fbc3382468ab1a87a
Lisha Zhou <lishazhou@Lishas-MacBook-Air.local>
2018-05-29 13:42:41 -0400

    add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,25 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+
+  // deleteMany
+  // db.collection('Todos').deleteMany({text: 'Eat Lauch'}).then((result) => {
+  //   console.log(result);
+  // });
+  // deleteOne
+  // db.collection('Todos').deleteOne({text: 'Eat Lauch'}).then((result) => {
+  //    console.log(result);
+  //  });
+
+  // findOneAndDelete
+  db.collection('Todos').findOneAndDelete({completed: false}).then((doc) => {
+    console.log(doc);
+  });
+
+  //db.close();
+});
