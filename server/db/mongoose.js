7618e78efade48058eea1829a30f32e92ea06e5e
Lisha Zhou <lishazhou@Lishas-MacBook-Air.local>
2018-06-01 11:29:32 -0400

    test on MONGODB_URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,15 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://zlisha0729:walds520_@ds245250.mlab.com:45250/todods');
-module.exports = {mongoose}
+//mongoose.connect('mongodb://zlisha0729:walds520_@ds245250.mlab.com:45250/todods');
+
+
+db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://zlisha0729:walds520_@ds245250.mlab.com:45250/todods'
+};
+
+mongoose.connect(process.env.MONGODB_URI|| db.localhost);
+
+
+module.exports = {mongoose};
