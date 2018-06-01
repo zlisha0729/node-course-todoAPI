26cb482cc1116e2d04bfe44815e88b948a258564
Lisha Zhou <lishazhou@Lishas-MacBook-Air.local>
2018-06-01 08:52:14 -0400

    refactor server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -25,7 +26,35 @@ app.get('/todos', (req, res) => {
     res.send({docs});
   }, (err) => {
     res.status(400).send(e);
-  })
+  });
+});
+
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+  else {
+    Todo.findById(id).then((doc) => {
+      if (!doc) {
+        res.status(404).send();
+      }
+      res.send({doc});  // ES 6 propert - send({docs: docs})
+    }, (err) => {
+      res.status(400).send();
+    })
+  }
+
+  // valid id using isValid
+    // 404 - send back empty Second
+
+    // findById
+      // succuess
+        // if todo - send it back
+        // if not todo - send back 404 with empty body
+      //error
+        // 400 - the request is not valid and send emepty body back
 });
 
 app.listen(3000, () => {
