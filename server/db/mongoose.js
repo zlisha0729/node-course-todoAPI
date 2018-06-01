fae5356e25f68d659ee884354be94bb3a8176ed3
Lisha Zhou <lishazhou@Lishas-MacBook-Air.local>
2018-06-01 10:09:17 -0400

    set up for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://zlisha0729:walds520_@ds245250.mlab.com:45250/todods');
 module.exports = {mongoose}
