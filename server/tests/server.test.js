302cf0a355c883826410166c7329ca9cb6732ad0
Lisha Zhou <lishazhou@Lishas-MacBook-Air.local>
2018-06-02 21:27:34 -0400

    test patch

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const expect = require('expect');
 const request = require('supertest');
 const {ObjectID} = require('mongodb');
@@ -10,7 +11,9 @@ const todos = [{
   text: 'First test todo'
 }, {
   _id: new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed:true,
+  completedAt: 333
 }];
 
 
@@ -132,5 +135,51 @@ describe('DELETE /todos/:id', () => {
       .delete('/todos/123')
       .expect(404)
       .end(done);
+  })
+});
+
+describe('PATCH /todos/:id', () => {
+  it('should update the todo', (done) => {
+    // grab id of first item
+    var hexId = todos[0]._id.toHexString();
+    // update text, set completed true
+    var text = 'this is the new text';
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: true,
+        text: text
+      })
+    //200
+      .expect(200)
+    //text is changed, completed is true, completedAt is a number .toBeA
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+        .end(done);
+  });
+
+  it('should clear completedAt when todo it not completed', (done) => {
+    // grab id of second todo item
+    var hexId = todos[1]._id.toHexString();
+    // update text, set completed false
+    var text = 'New text';
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: false,
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toNotExist();
+      })
+      .end(done);
+    // 200
+    // text is changed, completed is false, completedAt is null .toNotExist()
   })
 })
