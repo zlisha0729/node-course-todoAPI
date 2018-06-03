6670f858c9c27085f05dc5d044f0eac4c2ecc585
Lisha Zhou <lishazhou@Lishas-MacBook-Air.local>
2018-06-02 21:46:09 -0400

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,12 +5,12 @@ mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://zlisha0729:walds520_@ds245250.mlab.com:45250/todods');
 
 
-db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://zlisha0729:walds520_@ds245250.mlab.com:45250/todods'
-};
+// db = {
+//   localhost: 'mongodb://localhost:27017/TodoApp',
+//   mlab: 'mongodb://zlisha0729:walds520_@ds245250.mlab.com:45250/todods'
+// };
 
-mongoose.connect(process.env.MONGODB_URI|| db.localhost);
+mongoose.connect(process.env.MONGODB_URI);
 
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
