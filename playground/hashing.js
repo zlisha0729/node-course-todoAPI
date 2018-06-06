e0acedb449ed59df6843cfc2b06b29d3327591a6
Lisha Zhou <lishazhou@Lishas-MacBook-Air.local>
2018-06-05 21:36:19 -0400

    hash the password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,11 +1,26 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// 10 is number of rounds for bcrypt
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+var hashedPassword = '$2a$10$z/2txm.dXMjxHxBIo0srTuWQDoavQ3JPs5VlVyEy5hwyMDJxwUq2G';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+})
+//
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
